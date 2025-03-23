8404b13059462088f36a228d09a3c73fbf668b13
Fidra Sofyan <fidrahartono@gmail.com>
2025-03-23 17:35:12 +0700

    bump deps

--- src/app/watch.ts
+++ src/app/watch.ts
@@ -1,4 +1,3 @@
-import { sql } from 'kysely';
 import { DEFAULT_REPLY_MARKUP } from '../constant';
 import { kysely } from '../database';
 import { TelegramRepository } from '../repository/telegram-repository';
